f28c94540fe7349357ebfbcbf7dde756f8a61430
chagit3266 <chagit3266@gmail.com>
2025-03-06 08:46:42 +0200

    Fixed permission issue

--- WebApi/Mock/obj/Debug/net8.0/Mock.AssemblyInfo.cs
+++ WebApi/Mock/obj/Debug/net8.0/Mock.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mock")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dabc4a7d1caaa9377e5adb2b25d12c4223cc7174")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mock")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mock")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
